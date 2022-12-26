76189b4ebf5741d41d74d9252a6d74e83747f983
yongsun-park <yongsun.park@outlook.com>
2022-12-26 09:18:55 +0000

    feat: add the book example in concurrency_test

--- concurrency_test/src/main.rs
+++ concurrency_test/src/main.rs
@@ -1,12 +1,28 @@
 use std::thread;
 use rayon::prelude::*;
+use std::time::Duration;
 
 fn main() {
 
+    the_book_ex_16_1();
     // thread_for_example();
     // thread_fuctional_example();
-    rayon_example();
+    // rayon_example();
 
+}
+
+fn the_book_ex_16_1() {
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
 }
 
 fn thread_for_example() {
